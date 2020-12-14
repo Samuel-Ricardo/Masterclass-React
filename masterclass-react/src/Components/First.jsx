bd744f8edc18dcaa94ec9a769688d1c208dea4c2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-14 16:39:54 -0300

    react fragment

--- masterclass-react/src/Components/First.jsx
+++ masterclass-react/src/Components/First.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default () => 
 
-<>
+<React.Fragment>
         <h1>Primeiro componente 2</h1>
         <h2>Exemplo de um component React</h2>
-</>
+</React.Fragment>
 
