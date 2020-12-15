e72226b8e66b8021e802dfe82fd2450db882b538
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-15 15:51:30 -0300

    use children

--- masterclass-react/src/index.js
+++ masterclass-react/src/index.js
@@ -5,6 +5,7 @@ import './index.css'
 
 import First from './Components/First'
 import ComponentParameter from './Components/ComponentParameter'
+import ChildrenComp from './Components/ChildrenComp'
 
 ReactDOM.render(
 
@@ -14,7 +15,30 @@ ReactDOM.render(
 
         <ComponentParameter title='Segundo Titulo Bombástico'/>*/}
 
-        
+        <ChildrenComp>
+            <ul>
+                <li>
+                    Samuel
+                </li>
+
+                <li>
+                    Otávio
+                </li>
+
+                <li>
+                    Petra
+                </li>
+
+                <li>
+                    Julia
+                </li>
+
+                <li>
+                    Pedro
+                </li>
+            </ul>
+        </ChildrenComp>
+
     </div>
     ,
     document.getElementById('root')
