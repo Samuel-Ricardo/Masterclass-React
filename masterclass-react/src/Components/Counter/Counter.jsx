d8f8191af016fd9021f408a7116553b806b8e650
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-16 20:12:50 -0300

    Create: Class - Counter

---
+++ masterclass-react/src/Components/Counter/Counter.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default class contador extends React.Component {
+
+    render(){
+
+        return(
+
+            <div>
+
+                <header>
+                    <h2>Contador</h2>
+                    <h4>Display</h4>
+                </header>
+
+                <section>
+                    <button>+</button>
+                    <button>-</button>
+                </section>
+
+            </div>
+            
+        )
+    }
+
+        
+}
