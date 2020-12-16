1fa115c6a1d4a77c07180cdf89fac2b0a9a09931
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-16 20:01:10 -0300

    Input name

--- masterclass-react/src/Components/Input/Form.jsx
+++ masterclass-react/src/Components/Input/Form.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 export default porps => {
 
-    const name = 'pedro'
+    const [name,setName] = React.useState('pedro')
 
     return(
 
-        <input type='text' value = {name} readOnly></input>
+        <>
+            <h4>{name}</h4>
+
+            <input type='text' value = {name} 
+                
+                onChange={
+                    e => setName(e.target.value)
+                }></input>
+
+        </>
     )
 }
