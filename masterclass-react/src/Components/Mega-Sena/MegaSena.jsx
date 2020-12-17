aceda62af0318c5aa30ea4f4983fa1e813ac46b3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-17 19:47:06 -0300

    Create:  function -generateNumbersThatDontExist(numbers)

--- masterclass-react/src/Components/Mega-Sena/MegaSena.jsx
+++ masterclass-react/src/Components/Mega-Sena/MegaSena.jsx
@@ -5,6 +5,22 @@ export default props => {
 
     const [numbers, setNumbers] = React.useState(Array(props.quantidadeDeNumeros).fill(0))
 
+
+    function generateNumbersThatDontExist(numbers){
+
+        const min = props.min || 1;
+        const max = props.max || 100;
+
+        const newNumber = parseInt( Math.random() * (max - min)) + min
+
+        return numbers.includes(newNumber) 
+        
+        ? generateNumbersThatDontExist(numbers)
+
+        : generateNewNumber()
+    }
+
+
     return(
 
         <div>        
@@ -13,7 +29,7 @@ export default props => {
 
             <h3> {numbers.join(' ')}</h3>
 
-            <button> Sortear </button>
+            <button onCLick={this.generateNubers}> Sortear </button>
 
         </div>
     )
